e5fd4d6ce392e0cb798f4838032dc477a0b64973
furenku <furenku@debianT410.foocoop.mx>
2013-08-28 01:01:50 -0500

    multi checkbox meta

--- backend/metaboxes.php
+++ backend/metaboxes.php
@@ -79,23 +79,22 @@ add_action( 'save_post', 'proyecto_save_postdata' );
 /* Prints the taxonomy box content */
 function fechas_taller_meta_box($post) {
 
-    $dias = array ( 'L', 'M', 'M', 'J', 'V', 'S', 'D' );
+  $nombres_dias = array ( 'lunes', 'martes', 'miércoles', 'jueves', 'viernes', 'sabado', 'domingo' );
 
-    wp_enqueue_script( 'jquery-ui-datepicker' );
-    wp_enqueue_style( 'jquery-ui-style', 'http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.1/themes/smoothness/jquery-ui.css', true);
+  wp_enqueue_script( 'jquery-ui-datepicker' );
+  wp_enqueue_style( 'jquery-ui-style', 'http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.1/themes/smoothness/jquery-ui.css', true);
 
-    var_dump( get_post_meta( $post -> ID ) );
+  $fecha_inicio = get_post_meta( $post->ID, 'fecha_inicio', true  );
+  $fecha_final = get_post_meta( $post->ID, 'fecha_final', true  );
+  $dias_checked = get_post_meta( $post->ID, 'dias_semana', true  );
 
-    $fecha_inicio = get_post_meta( $post->ID, 'fecha_inicio', true  );
-    $fecha_final = get_post_meta( $post->ID, 'fecha_final', true  );
-    
-    wp_nonce_field( plugin_basename( __FILE__ ), 'fechas_taller_noncename' );
+  wp_nonce_field( plugin_basename( __FILE__ ), 'fechas_taller_noncename' );
 
 ?>
 
-<div id="dias_semana_div">
+    <form id="fechas_form">
+  <div id="fechas">
 
-<div id="fechas">
 Del
 <input class="fecha" name="fecha_inicio" type="textbox" value="<?php echo $fecha_inicio; ?>"/>
 </br>
@@ -103,15 +102,16 @@ al
 <input class="fecha" name="fecha_final"  type="textbox" value="<?php echo $fecha_final; ?>"/>  
 </div>
 
+<div id="dias_semana_div">
     <?php 
-    foreach( $dias as $dia ) {
-        $echo .= '<label><input type="checkbox" name="'.$dia.'">'.$dia.'</label>';
+    foreach( $nombres_dias as $dia ) {
+      $echo .= '<label><input type="checkbox" name="dias_semana[]" value="'.$dia.'" '. (in_array($dia,$dias_checked ) ? 'checked':'' ).'>'.$dia.'</label>';
     }
 
     echo $echo;
     ?>
-
 </div>
+</form>
 
 
 <script>
@@ -128,28 +128,34 @@ jQuery(document).ready(function(){
 
 function fechas_taller_save_postdata( $post_id ) {
 
-    if ( ( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE ) || wp_is_post_revision( $post_id ) ) 
+  if ( ( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE ) || wp_is_post_revision( $post_id ) ) 
+  return;
+
+  if ( !wp_verify_nonce( $_POST['fechas_taller_noncename'], plugin_basename( __FILE__ ) ) )
+  return;
+
+  if ( 'taller' == $_POST['fechas_taller_type'] ) 
+  {
+    if ( !current_user_can( 'edit_page', $post_id ) )
     return;
-
-    if ( !wp_verify_nonce( $_POST['fechas_taller_noncename'], plugin_basename( __FILE__ ) ) )
+  }
+  else
+  {
+    if ( !current_user_can( 'edit_post', $post_id ) )
     return;
+  }
 
-    if ( 'taller' == $_POST['fechas_taller_type'] ) 
-    {
-        if ( !current_user_can( 'edit_page', $post_id ) )
-        return;
-    }
-    else
-    {
-        if ( !current_user_can( 'edit_post', $post_id ) )
-        return;
-    }
+  $fecha_inicio = $_POST['fecha_inicio'];
+  $fecha_final = $_POST['fecha_final'];
+  
+  $dias = $_POST['dias_semana'];
+  /* $dias = array( $_POST['lunes'], $_POST['martes'], $_POST['miércoles'], $_POST['jueves'], $_POST['viernes'] ); */
 
-    $fecha_inicio = $_POST['fecha_inicio'];
-    $fecha_final = $_POST['fecha_final'];
-    
-    add_post_meta($post_id, 'fecha_inicio', $fecha_inicio, true );
-    add_post_meta($post_id, 'fecha_final', $fecha_final, true );
+  
+  
+  add_post_meta($post_id, 'dias_semana', $dias, true );
+  add_post_meta($post_id, 'fecha_final', $fecha_final, true );
+  add_post_meta($post_id, 'fecha_final', $fecha_final, true );
 
 }
 
